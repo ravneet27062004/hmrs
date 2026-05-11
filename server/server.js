fae8d7dda9949e44158ddd94a44d413a2826482e
ravneet kaur <146877258+ravneet27062004@users.noreply.github.com>
2026-05-11 11:32:37 +0530

    server cahnges

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+app.options("*", cors());
 app.use(express.json());
 
 mongoose
@@ -33,7 +34,11 @@ app.use("/api/employees", employeeRoutes);
 app.use("/api/attendance", attendanceRoutes);
 app.use("/api/leaves", leaveRoutes);
 app.use("/api/dashboard", dashboardRoutes);
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
 module.exports = app;
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
